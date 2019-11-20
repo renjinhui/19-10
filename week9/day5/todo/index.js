88ac0914c3968f170592b4fc3912827660b9c6ef
renjinhui <1227760869@qq.com>
2019-11-20 18:15:09 +0800

    m

--- week9/day5/todo/index.js
+++ week9/day5/todo/index.js
@@ -8,30 +8,45 @@ Vue.directive('focus',function(el,obj){
 let vm = new Vue({
     el: '#app',
     data: {
-        ary: [{
-            id: 1,
-            todo: '吃饭',
-            done: false,
-            editable: false
-        }, {
-            id: 2,
-            todo: '睡觉',
-            done: false,
-            editable: false
-        }, {
-            id: 3,
-            todo: '打豆豆',
-            done: true,
-            editable: false
-        }],
+        ary: [],
         todo:'',
+        count:0,
         hash:'',// 用来存储当前路径的hash值
+    },
+    computed: {
+        todoAry(){
+            // 未完成的事情的个数， 因为只要数组发生改变  count就要重新赋值
+            this.count = this.ary.filter(item=>!item.done).length;
+
+            // 只要this.ary发生改变  就要把数据存储到本地
+            localStorage.setItem('mytodolist',JSON.stringify(this.ary))
+
+            // 依赖于 ary  依赖于 hash
+            switch (this.hash) {
+                case '#/all':
+                    // 若是全部列表  则把整个数组返回
+                    return this.ary
+                    break;
+                case '#/finished':
+                    // 若是完成列表 则返回 ary中 done属性是true的项
+                    return this.ary.filter(item=>item.done)
+                    break;
+                case '#/unfinished':
+                    return this.ary.filter(item=>!item.done)
+                    break    
+                default:
+                    break;
+            }
+        }
     },
     created() {
         this.hash = location.hash || '#/all';
         window.addEventListener('hashchange',()=>{
             this.hash = location.hash;
-        })
+        });
+        // 从本地存储中获取数据
+        this.ary = JSON.parse(localStorage.getItem('mytodolist'))||[];
+
     },
     methods: {
         submit(){
@@ -49,8 +64,19 @@ let vm = new Vue({
         change(obj){
             obj.editable = !obj.editable
         },
-        del(n){
-            this.ary.splice(n,1)
+        del(obj){
+            this.ary = this.ary.filter(item=>item.id!==obj.id)
+        }
+    },
+    watch: {
+        /* ary(){
+            console.log(666)
+        } */
+        ary:{
+            deep:true,
+            handler(newV,oldV){
+                console.log(666)
+            }
         }
     },
 })
